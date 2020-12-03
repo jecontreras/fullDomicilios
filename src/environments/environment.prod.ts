7f0c3ce99103ba12b1f73a14ea2ef230552033fa
jecontreras <joseeduar147@gmail.com>
2020-12-03 16:26:27 -0500

    cambios new

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,7 +9,7 @@ export const environment = {
   urlServer: "https://gpssockets.herokuapp.com",
   urlActivacion: "https://www.google.com",
   mapbox: {
-    accessTokens: "pk.eyJ1IjoiZXhhbXBsZXMiLCJhIjoiY2p0MG01MXRqMW45cjQzb2R6b2ptc3J4MSJ9.zA2W0IkI0c6KaAhJfk9bWg"
+    accessTokens: ""
   },
   urlFront: "https://gpsmaster-d6ee7.firebaseapp.com/",
   vercionApp: "1.0"
